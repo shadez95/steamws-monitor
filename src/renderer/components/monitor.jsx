a43a844222cff20019130fb486ecca78f96ed3be
dixon <dixonbegay@gmail.com>
2017-03-15 16:37:22 -0700

    Added some html that will be in future component

--- src/renderer/components/monitor.jsx
+++ src/renderer/components/monitor.jsx
@@ -7,6 +7,14 @@ class MonitorPage extends Component {
       <div id="MonitorPage">
         <br />
         <h3>This will be the monitoring page</h3>
+        <Form>
+          <FormGroup row className="container">
+            <Label for="localLocationID">Local Install Location:</Label>
+            <Col>
+              <Input type="text" name="localLocation" id="localLocationID" placeholder="C:\location\to\workshop\file\folder" />
+            </Col>
+          </FormGroup>
+        </Form>
       </div>
     )
   }
