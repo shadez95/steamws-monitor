986bcbd383974f369007ab46b7683a44fa5abf45
dixon <dixonbegay@gmail.com>
2017-03-16 18:52:40 -0700

    Added workshopInput state to monitor text input

--- src/renderer/components/add.jsx
+++ src/renderer/components/add.jsx
@@ -4,6 +4,18 @@ import { Button, Form, FormGroup, Label, Input, FormText, Col } from 'reactstrap
 // CBA workshop ID: 450814997
 
 export default class AddPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {workshopInput: ''}
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInput = this.handleInput.bind(this)
+  }
+  handleInput(e) {
+    this.setState({workshopInput: e.target.value})
+  }
+  handleSubmit() {
+    console.log(this.state.workshopInput)
+  }
   render() {
     return (
       <div id="AddPage">
@@ -12,10 +24,10 @@ export default class AddPage extends Component {
           <FormGroup row className="container">
             <Label for="workshopID">Steam Workshop ID:</Label>
             <Col>
-              <Input type="text" name="workshopIDitem" id="workshopID" placeholder="123456789" />
+              <Input type="text" name="workshopIDitem" onChange={this.handleInput} id="workshopID" placeholder="123456789" />
             </Col>
           </FormGroup>
-          <Button color="success" type="submit" value="Submit">Add</Button>
+          <Button color="success" type="submit" onClick={this.handleSubmit} value="Submit">Add</Button>
 
       </div>
     )
