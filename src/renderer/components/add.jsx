1122034a76c601d35219e613634fb52563615790
dixon <dixonbegay@gmail.com>
2017-03-16 18:29:19 -0700

    Added cba workshop id for reference

--- src/renderer/components/add.jsx
+++ src/renderer/components/add.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Button, Form, FormGroup, Label, Input, FormText, Col } from 'reactstrap'
 
+// CBA workshop ID: 450814997
+
 export default class AddPage extends Component {
   render() {
     return (
@@ -10,11 +12,11 @@ export default class AddPage extends Component {
           <FormGroup row className="container">
             <Label for="workshopID">Steam Workshop ID:</Label>
             <Col>
-              <Input type="text" name="workshopIDitem" id="workshopID" placeholder="450814997" />
+              <Input type="text" name="workshopIDitem" id="workshopID" placeholder="123456789" />
             </Col>
           </FormGroup>
           <Button color="success" type="submit" value="Submit">Add</Button>
-        
+
       </div>
     )
   }
