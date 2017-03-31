1637f008f2940dd236478051ddbdb982725ee26f
Dixon Begay <dixonbegay@gmail.com>
2017-03-30 23:54:20 -0700

    Fixed tabbing

--- src/renderer/main/components/main.jsx
+++ src/renderer/main/components/main.jsx
@@ -44,32 +44,32 @@ class Main extends Component {
   }
   render() {
     return (
-        <SplitPane split="vertical" primary="first" defaultSize={200}>
-          <div>
-            <nav className="hidden-xs-down bg-faded sidebar">
-              <Nav vertical pills >
-                <NavbarBrand>Add a Game</NavbarBrand>
-                <NavbarBrand>Components</NavbarBrand>
-                <NavItem>
-                  <NavLink active href="/components/">Components</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="#">Testing length in nav</NavLink>
-                </NavItem>
-                <NavbarBrand>reactstrap</NavbarBrand>
-                <NavItem>
-                  <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-                </NavItem>
-              </Nav>
-            </nav>
+      <SplitPane split="vertical" primary="first" defaultSize={200}>
+        <div>
+          <nav className="hidden-xs-down bg-faded sidebar">
+            <Nav vertical pills >
+              <NavbarBrand>Add a Game</NavbarBrand>
+              <NavbarBrand>Components</NavbarBrand>
+              <NavItem>
+                <NavLink active href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Testing length in nav</NavLink>
+              </NavItem>
+              <NavbarBrand>reactstrap</NavbarBrand>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+              </NavItem>
+            </Nav>
+          </nav>
+        </div>
+        <div>
+          <div className="container-fluid">
+            <h2>Test Title</h2>
+            <p>Test text</p>
           </div>
-          <div>
-            <div className="container-fluid">
-              <h2>Test Title</h2>
-              <p>Test text</p>
-            </div>
-          </div>
-        </SplitPane>
+        </div>
+      </SplitPane>
     )
   }
 }
