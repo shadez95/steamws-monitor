8e9eb18d8ae493b180c29fdb5eb5b11ccb6df676
dixon <dixonbegay@gmail.com>
2017-04-18 14:51:17 -0700

    Removed unnecessary imports

--- src/renderer/main/containers/main.jsx
+++ src/renderer/main/containers/main.jsx
@@ -1,14 +1,9 @@
 import React, { Component }  from "react";
-import { NavItem, NavLink, NavbarBrand } from "reactstrap";
-import { connect } from "react-redux";
 
 import SplitPane from "react-split-pane";
 
 import CustomNav from "./nav";
 import PaneContent from "./paneContent";
-
-import AddGame from "../components/addGame";
-import Settings from "../components/settings";
 
 // {[
 //   {
