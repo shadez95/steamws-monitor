c0f58d52d9ba2e9ba3bdf6b6aa2718ef5fe6a4ce
dixon <dixonbegay@gmail.com>
2017-04-24 14:46:48 -0700

    Added deleteConfig function

--- src/renderer/store/configManipulators.js
+++ src/renderer/store/configManipulators.js
@@ -1,9 +1,11 @@
 const remote = require("electron").remote;
 const config = remote.require("electron-settings");
 
+const options = { prettify: true };
+
 export async function changeConfig(obj, value) {
   // emit loading event here to display loading icon
-  config.set(obj, value);
+  config.set(obj, value, options);
   // emit not loading event here to remove loading icon
 }
 
@@ -12,4 +14,8 @@ export function getConfig(obj, defaultValue="") {
   const val = config.get(obj, defaultValue);
   // emit not loading event here to remove loading icon
   return val;
+}
+
+export function deleteConfig(keyPath) {
+  config.delete(keyPath, options);
 }
