fc1711507b6a3c7e9a186e36173b32a1ee4d8c9a
Dixon Begay <dixonbegay@gmail.com>
2017-10-28 21:24:23 -0700

    Changed a var to a const

--- app/app.js
+++ app/app.js
@@ -234,7 +234,7 @@ const openSteamWSWindow = () => {
   }
 };
 
-var shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory) {
+const shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory) {
   // Someone tried to run a second instance, we should focus our window
   if (mainWindow) {
     if (mainWindow.isMinimized()) mainWindow.restore();
